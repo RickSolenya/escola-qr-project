955a714f9de3d0724524d6687efee9756bc747ed
RickSolenya <81687356+RickSolenya@users.noreply.github.com>
2026-05-18 18:42:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/* =========================
+   SWIPER
+========================= */
+
+const swiper = new Swiper(".mySwiper", {
+
+    loop: true,
+
+    spaceBetween: 20,
+
+    centeredSlides: true,
+
+    pagination: {
+
+        el: ".swiper-pagination",
+
+        clickable: true,
+    },
+
+    navigation: {
+
+        nextEl: ".swiper-button-next",
+
+        prevEl: ".swiper-button-prev",
+    },
+
+});
